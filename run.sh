7d50213fd627e82f8074dd53b851823c151e6a2e
EC2 Default User <ec2-user@ip-172-31-37-99.ap-northeast-1.compute.internal>
2023-09-19 11:04:50 +0000

    20230919_2004

---
+++ run.sh
@@ -0,0 +1,19 @@
+IMAGE_NAME="kaggle_pytorch:1.11.0-cuda11.3-cudnn8-runtime"
+
+docker run --rm -it \
+    --privileged \
+    --gpus all \
+    --shm-size 50G \
+    -e LOCAL_UID=$(id -u $USER) \
+    -e LOCAL_GID=$(id -g $USER) \
+    -e PYTHONPATH=/home/developer/transformers:/home/developer/transformers/experiments/minigrid/dependencies/gym-minigrid:/home/developer/transformers/experiments/minigrid/dependencies/torch-ac \
+    -v .:/home/jovyan/kaggle_pytorch_docker\
+    --net host \
+    -e USE_VNC=true \
+    -e DISPLAY=:5 \
+    -e BUSID=$(nvidia-xconfig --query-gpu-info | grep BusID | sed -n 1p | cut -d' ' -f 6) \
+    -e SCREEN_RESOLUTION=1280x1024x24 \
+    -e VNC_PASSWORD=passpass \
+    -p 8888:8888 \
+    --name $(id -u -n)-kaggle_pytorch \
+    ${IMAGE_NAME}
